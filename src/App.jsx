0c861fbe749e3935e9d7a1b6ed7938cda4081a9e
marimuthu37 <marimuthug037@gmail.com>
2025-02-14 16:55:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import StudentForm from "./StudentForm";
 import "./index.css";
